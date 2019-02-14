58e421b6da74dcf8003c1f1802bf09e4308aa365
Yurihel <42873299+Yurihel@users.noreply.github.com>
2019-02-14 11:29:27 -0600

    Create reto2dadocaras.js

---
+++ reto2dadocaras.js
@@ -0,0 +1,42 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  if (mouseIsPressed);
+  rect(150, 150, 100, 100);
+
+  ellipse(200, 200, 20, 20);
+
+
+  ellipse(170, 170, 20, 20);
+  ellipse(230, 230, 20, 20);
+
+
+  ellipse(170, 170, 20, 20);
+  ellipse(230, 230, 20, 20);
+  ellipse(200, 200, 20, 20);
+
+
+  ellipse(170, 170, 20, 20);
+  ellipse(170, 230, 20, 20);
+  ellipse(230, 170, 20, 20);
+  ellipse(230, 230, 20, 20);
+
+
+  ellipse(170, 170, 20, 20);
+  ellipse(170, 230, 20, 20);
+  ellipse(230, 170, 20, 20);
+  ellipse(230, 230, 20, 20);
+  ellipse(200, 200, 20, 20);
+
+
+  ellipse(170, 170, 20, 20);
+  ellipse(170, 230, 20, 20);
+  ellipse(230, 170, 20, 20);
+  ellipse(230, 230, 20, 20);
+  ellipse(170, 200, 20, 20);
+  ellipse(230, 200, 20, 20);
+
+}
